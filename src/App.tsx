5509e34b1cd9f716482c7e7cd0c33c56d7d5186b
Shunsuke Hioki <iron.kicka624@gmail.com>
2021-11-12 13:45:59 +0900

    add apigw & UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import {AmplifySignOut, withAuthenticator} from "@aws-amplify/ui-react";
-import config from "./aws-exports"
-import {Amplify} from "aws-amplify";
-Amplify.configure(config)
-function App() {
-  return (
-   <div>
-     <h1>Hello from AWS Amplify</h1>
-     <AmplifySignOut/>
-   </div>
-  );
+import {API} from "aws-amplify";
+
+type CoinInfo = {
+    name:string;
+    symbol:string;
+    price_usd:string;
 }
 
-export default withAuthenticator(App);
+function App() {
+    const [coins, updateCoins] = useState<CoinInfo[]>([])
+
+    async function fetchCoins(){
+        const data = await API.get('cryptoapi','/coins',{});
+        updateCoins(data.coins)
+    }
+
+    useEffect(()=>{
+        fetchCoins();
+    },[])
+
+    return (
+        <div className={"App"}>
+            {
+                coins.map((coin,index)=>(
+                    <div key={index}>
+                        <h2>{coin.name} - {coin.symbol}</h2>
+                        <h5>${coin.price_usd}</h5>
+                    </div>
+                ))
+            }
+        </div>
+    );
+}
+
+export default App;
