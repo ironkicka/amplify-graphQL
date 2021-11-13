effd5ed84fadbb736d8e5f5bc5b1f9755d2906d6
Shunsuke Hioki <iron.kicka624@gmail.com>
2021-11-13 11:30:14 +0900

    add top page

--- notesapp/src/App.js
+++ notesapp/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useReducer} from "react";
+import {API} from "aws-amplify";
+import {listNotes} from "./graphql/queries";
+import {List} from "antd";
+
+const initialState = {
+    notes: [],
+    loading: true,
+    error: false,
+    form: {name: '', description: ''}
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "SET_NOTES":
+            return {...state, notes: action.notes, loading: false}
+        case 'ERROR':
+            return {...state, loading: false, error: true}
+        default:
+            return state;
+    }
+};
+const styles = {
+    container: {padding: 20},
+    input: {marginBottom: 10},
+    item: {textAlign: 'left'},
+    p: {color: '#1890FF'}
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const fetchNotes = async () => {
+        try {
+            const notesData = await API.graphql({
+                query: listNotes
+            })
+            dispatch({type: 'SET_NOTES', notes: notesData.data.listNotes.items})
+        } catch (err) {
+            console.log(err);
+            dispatch({type: 'ERROR'});
+        }
+    }
+
+    const renderItem = (item) => {
+        return (
+            <List.Item style={styles.item}>
+                <List.Item.Meta
+                    title={item.name}
+                    description={item.description}
+                />
+            </List.Item>
+        )
+    }
+    useEffect(() => {
+        fetchNotes();
+    }, [])
+
+    return (
+        <div style={styles.container}>
+            <List
+                loading={state.loading}
+                dataSource={state.notes}
+                renderItem={renderItem}
+            />
+        </div>
+    );
 }
 
 export default App;
